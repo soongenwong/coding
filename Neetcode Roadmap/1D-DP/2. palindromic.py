9ac5f6695b2e54d996e273f2d1103e23f038a361
Soong En Wong <wsoongen2015@gmail.com>
2025-06-25 01:10:22 +0100

    Initial commit of coding practice files

---
+++ Neetcode Roadmap/1D-DP/2. palindromic.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n, res = len(s), 0
+        dp = [[False] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    res += 1
+
+        return res
+
+        #dp solution
+        #time complexity: O(n^2)
+        #space complexity: O(n^2)
