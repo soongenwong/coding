83f74d370cbf2ae69dff7cf62fb50d859e875b06
Soong En Wong <wsoongen2015@gmail.com>
2025-08-25 22:50:06 +0100

    first question in array

---
+++ Leetcode/Arrays/search-insert-position.py
@@ -0,0 +1,32 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+        
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else: 
+                left = mid + 1
+        
+        return left
+
+
+
+
+
+        # initial plan
+        # use binary search to find the target value.
+        # if the target value is not found, find the closest values and insert the target value. 
+
+        # final solution
+        # correct solution!
+        # initialise the left and right positions. 
+        # find the mid point. 
+        # if mid = target, return mid
+        # if mid > target, right = mid - 1
+        # if mid < target, left = mid + 1
