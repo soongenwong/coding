3ef605362dda26b62c9883edc45f4ce21a452c65
Soong En Wong <wsoongen2015@gmail.com>
2025-08-26 11:59:35 +0100

    second medium question in array

---
+++ Leetcode/Arrays/longest-substring.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = max_length = 0
+        char_set = set()
+        
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
+
+
+
+        # initial plan
+        # search if the values are different
+        # if they are different, add to the output
+        # if the values are the same, move to the next value. 
+        # return the number with the highest different values. 
+
+        # final solution
+        # use sliding window and set
+        # left is the pointer, max_length is the value to return
+        # char_set holds the unique characters currently in the window. 
+        # while s[right] in char set, there is a duplicate in the window, so remove s[left] 
+        # add s[right] to char set
+        # update max length
+        
