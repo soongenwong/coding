2d2598a72b52a27235911a07a3b2de7e33eb8283
Soong En Wong <wsoongen2015@gmail.com>
2025-08-27 18:15:30 +0100

    first question in strings

---
+++ Leetcode/Strings/reverse-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
+
+
+
+
+        # arrays: stores numbers, strings or any collection of items
+        # strings: stores words, letters ot sentences
+
+        # initial plan
+        # store the values in a stack and pop the values out
+
+        # final solution
+        # s[::-1] creates a reversed version of the list s
+        # s[:] replace the contents of s with the contents on the right
+        # s[::-1] , s[start:stop:step], no start or stop, so go from beginning to end, -1 so move backwards through the list.
+
+
+     
+
+        
